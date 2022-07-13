b4fae6bd7bc15ad76790711925631f930e2f2d93
Mihael Bun <bunmihail@gmail.com>
2022-07-14 02:25:30 +0300

    Task 2 complete add+1

--- HomeWork/Task 2/Program.cs
+++ HomeWork/Task 2/Program.cs
@@ -4,14 +4,28 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.WriteLine("Enter 2 numbers and we'll tell you which one is bigger.");
 Console.Write("Write namber A: ");
 int numberA = int.Parse(Console.ReadLine());
 
 Console.Write("Write namber B: ");
 int numberB = int.Parse(Console.ReadLine());
 int max = 0;
+int min = 0;
 
-if (numberA > numberB) max = numberA;
-else if (numberA < numberB) max = numberB;
-else if (numberB == numberA) max = numberB = numberA;
-Console.WriteLine(max);
+if (numberA > numberB) 
+{
+    max = numberA; min = numberB;
+    Console.WriteLine($"The number {max} is bigger than the {min}");
+}
+else if (numberA < numberB) 
+{
+    max = numberB; min = numberA;
+    Console.WriteLine($"The number {max} is bigger than the {min}");
+}
+else
+{
+    Console.WriteLine($"Both numbers {max} and {min} are equal");
+}
+
+
