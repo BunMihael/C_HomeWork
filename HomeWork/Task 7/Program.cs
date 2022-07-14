58a32bf3d1a40617a0680e291de1154b0e64d85d
Mihael Bun <bunmihail@gmail.com>
2022-07-14 20:29:34 +0300

    Task 7 is complete

---
+++ HomeWork/Task 7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Please write a three digit number and we will show a last number.");
+Console.Write("Write three digit number here: ");
+int number = int.Parse(Console.ReadLine());
+int LastNumb = 0;
+
+LastNumb = number % 10;
+Console.WriteLine($"The number {LastNumb} is the last number of {number}. ");
