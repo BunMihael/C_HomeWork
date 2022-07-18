d6a78ac7413df711d8252e1c7698bc1f992adcc8
Mihael Bun <bunmihail@gmail.com>
2022-07-18 20:52:13 +0300

    Task 1 complete

---
+++ C_HomeWork2/Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Please write a three digit number and we will show a middle number.");
+Console.Write("Write three digit number here: ");
+int number = int.Parse(Console.ReadLine());
+int MidNum = 0;
+
+MidNum = number % 100;
+MidNum = LastNumb / 10;
+Console.WriteLine($"The number {MidNum} is the last number of {number}. ");
