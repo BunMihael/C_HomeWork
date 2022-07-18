bb4e2a054e2f5be74b0b83654e0d42f3f1a6bc08
Mihael Bun <bunmihail@gmail.com>
2022-07-19 00:29:58 +0300

    Task 2 complete

---
+++ C_HomeWork2/Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int a = new Random().Next(100,1000);
+int prt1 = 0;
+int prt2 = 0;
+
+prt1 = a / 100;
+prt2 = a % 10;
+
+Console.WriteLine($"Number {a} without middle number is looking like this: {prt1}{prt2} ");
