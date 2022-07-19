20a98e8bc4ed343f056655f23f319590bc938672
Mihael Bun <bunmihail@gmail.com>
2022-07-19 08:01:21 +0300

    Task 3 start

--- C_HomeWork2/Task 3/Program.cs
+++ C_HomeWork2/Task 3/Program.cs
@@ -3,3 +3,15 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 100)
+    {
+        Console.WriteLine("There is no third digit number");
+    }
+    while (num > 999) num = num/ 10;
+    Console.WriteLine(num % 10);
+}
+ThirdNum(int.Parse(Console.ReadLine()));
