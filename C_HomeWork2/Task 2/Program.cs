7ac7bdeee0c444f73dc90645b9ae3abbac9d621a
Mihael Bun <bunmihail@gmail.com>
2022-07-19 08:01:08 +0300

    Task 2 start++

--- C_HomeWork2/Task 2/Program.cs
+++ C_HomeWork2/Task 2/Program.cs
@@ -4,7 +4,7 @@
 // 782 -> 72
 // 918 -> 98
 
-int a = new Random().Next(100,1000);
+int a = new Random().Next(100, 1000);
 int prt1 = 0;
 int prt2 = 0;
 
@@ -12,3 +12,18 @@ prt1 = a / 100;
 prt2 = a % 10;
 
 Console.WriteLine($"Number {a} without middle number is looking like this: {prt1}{prt2} ");
+
+// void.NumRendom()
+//{
+//    int num = new.Random().Next(100,1000);
+//    Console.WriteLine($"{num} -> {num/ 100}{num% 10}");
+//}
+// NumRendom();
+
+
+//// void.NumRendom()
+//{
+//    int num = new.Random().Next(100,1000);
+//    Console.Write($"{num} -> {num/ 100 *10 + num % 10}");
+//}
+// NumRendom();
