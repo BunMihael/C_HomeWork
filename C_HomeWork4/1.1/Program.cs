ec4432599a652bc552b4d2355629b7e0e430e7f0
Mihael Bun <bunmihail@gmail.com>
2022-08-09 20:13:58 +0300

    Task 1.1 complete

---
+++ C_HomeWork4/1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+void NaturDegree(int a, int b)
+{
+    int nums = 1;
+    for (int i = 1; i <= b; i++)
+    { 
+        nums = nums * a;
+    }
+    Console.Write($"{a} в натуральной степени {b} = {nums}");
+}
+NaturDegree(3, 5);
