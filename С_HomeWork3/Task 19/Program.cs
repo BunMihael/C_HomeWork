a05709d18081af3ee744af3b3c599e2badd3f15f
Mihael Bun <bunmihail@gmail.com>
2022-08-09 08:30:44 +0300

    Task Task 19 complete 2.0

--- С_HomeWork3/Task 19/Program.cs
+++ С_HomeWork3/Task 19/Program.cs
@@ -3,11 +3,34 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число и мы скажем полиндром ли оно: ");
-string num = Console.ReadLine();
-if (num!.Length == 5)
+//Console.WriteLine("Введите пятизначное число и мы скажем полиндром ли оно: ");
+//string num = Console.ReadLine();
+//if (num!.Length == 5)
+//{
+//    string rev = string.Concat(num.Reverse());
+//    Console.WriteLine(num == rev ? "Число палиндром" : "Число не палиндром");
+//}
+//else Console.WriteLine($"Вы ввели не пятизначное число");
+
+void Polindrom(int n)
 {
-    string rev = string.Concat(num.Reverse());
-    Console.WriteLine(num == rev ? "Число палиндром" : "Число не палиндром");
+    int i = 0;
+    string num = n.ToString();
+    int size = num.Length - 1;
+    string flag = "yes";
+
+    while(i<size/2)
+    {
+        if(num[i] != num[size - i])
+        {
+            flag = "no";
+            break;
+        }
+        i += 1;
+    }
+    Console.WriteLine($"{n} -> {flag}");
 }
-else Console.WriteLine($"Вы ввели не пятизначное число");
+
+Polindrom(12488421);
+Polindrom(23432);
+Polindrom(14212);
