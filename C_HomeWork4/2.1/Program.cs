5ebbf52e5d4a9f9d3b1cd2afa3d674c4abed3f6c
Mihael Bun <bunmihail@gmail.com>
2022-08-10 20:50:31 +0300

    Task 3.1 complete

--- C_HomeWork4/2.1/Program.cs
+++ C_HomeWork4/2.1/Program.cs
@@ -21,3 +21,35 @@ void Sum(int a)
 Sum (452);
 Sum(82);
 Sum(9012);
+
+/*
+double DigitSum(int num)
+{
+    string n = num.ToString();
+    int length = n.Length;
+    double sum = 0;
+
+    for(int i = 0; i < length; i++)
+    {
+        double m = char.GetNumericValue(n[i]);
+        sum += m;
+    }
+
+    return sum;
+}
+
+int n = 123;
+
+Console.WriteLine(DigitSum(n));
+
+
+int SumNum(int num)
+{
+    int n_sum = 0;
+
+    for(int i = num; num != 0; num /= 10)
+    n_sum += num% 10;
+    return n_sum;
+}
+Console.WriteLine(SumNum(456));
+*/
--- C_HomeWork4/3.1/Program.cs
+++ C_HomeWork4/3.1/Program.cs
@@ -1,16 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 29: Напишите программу, которая задаёт массив из
+8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
 
 
-int [] EightMass()
+
+
+void PrintArray(int[] arr)
 {
-return arr;
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
 }
 
-void Print(int[] arr)
+int[] EightMass()
 {
-
+    int count = 8;
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
 }
-
-int arr_1 = EightMas();
-Print(arr_1);
+int[] arr_1 = EightMass();
+PrintArray(arr_1);
+int[] arr_2 = EightMass();
+PrintArray(arr_2);
