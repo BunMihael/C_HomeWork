fc828dc8b38b9cd4f83f40068822067a82896ae1
Mihael Bun <bunmihail@gmail.com>
2022-08-18 23:43:22 +0300

    Task 5.2 compete

---
+++ C_HomeWork5/Task 2/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 26
+[-4, -6, 89, 6] -> 0
+*/
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] GenerateNumbers(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(10, 100);
+    }
+    return arr;
+}
+
+int SumOfElements(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            count = count + arr[i];
+        }
+
+    }
+    return count;
+}
+
+int[] arr_1 = GenerateNumbers(4);
+Print(arr_1);
+Console.WriteLine(SumOfElements(arr_1));
