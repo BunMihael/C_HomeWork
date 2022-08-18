639fe7a5942e098981f35a24009599042f3c1ab2
Mihael Bun <bunmihail@gmail.com>
2022-08-18 23:16:58 +0300

    Task 5.1 compete

---
+++ C_HomeWork5/Task 1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] GenerateNumbers(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+int EvenNumber(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+        
+    }
+    return count;
+}
+
+int[] arr_1 = GenerateNumbers(15);
+Print(arr_1);
+Console.WriteLine(EvenNumber(arr_1));
