9dae34591a25ae09b7ada73ee06e4a86ef69f10e
Mihael Bun <bunmihail@gmail.com>
2022-09-08 22:57:19 +0300

    Task 9.1 and 9.2 complete

---
+++ C_HomeWork9/Task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Задайте значения M и N. Напишите программу, которая выведет все 
+// чётные натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""2, 4""
+// M = 4; N = 8. -> ""4, 6, 8""
+
+string EvenNaturalNumbers(int M, int N)
+{
+    if (M > N) return String.Empty;
+    else if (M % 2 != 1) return $"{M} " + EvenNaturalNumbers(M + 2, N);
+    else return EvenNaturalNumbers(M + 1, N);
+}
+Console.WriteLine(EvenNaturalNumbers(2, 5));
+Console.WriteLine(EvenNaturalNumbers(4, 8));
---
+++ C_HomeWork9/Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int NumbersRec(int a, int b)
+{
+    if (a >= b) return a;
+    else return a + NumbersRec(a + 1, b);
+}
+
+Console.WriteLine(NumbersRec(1, 15));
+Console.WriteLine(NumbersRec(4, 8));
+
