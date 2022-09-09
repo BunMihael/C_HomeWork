8497eda56a67ddcb4c971fd7867b035771df6d32
Mihael Bun <bunmihail@gmail.com>
2022-09-10 00:31:09 +0300

    Task 9.1 ++

--- C_HomeWork9/Task 1/Program.cs
+++ C_HomeWork9/Task 1/Program.cs
@@ -11,3 +11,16 @@ string EvenNaturalNumbers(int M, int N)
 }
 Console.WriteLine(EvenNaturalNumbers(2, 5));
 Console.WriteLine(EvenNaturalNumbers(4, 8));
+
+// void AllEvenNumers(int M, int N)
+// {
+// if (M > N) return;
+// if (M % 2 == 1) M += 1;
+
+// Console.Write($"{M}, ");
+// AllEvenNumers(M + 2, N);    
+// }
+
+// AllEvenNumers(1, 5);
+// Console.WriteLine();
+// AllEvenNumers(4, 8);
